ca2ad1bfa2f89cace6d0b212d1c90f363da0c119
febrianide <febriani.dewi.alviah@students.untidar.ac.id>
2026-03-10 07:57:22 +0700

    inicial CI pipeline

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('GET /', () => {
+  test('should return Hello CI Pipeline', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe('Hello CI Pipeline!');
+  });
+});
